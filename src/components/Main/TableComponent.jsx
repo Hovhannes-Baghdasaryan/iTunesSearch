47d04653fb1f2a9b775b363909f1679b22418fde
Hovhannes Baghdasaryan <hovhannes.baghdasaryan.03@gmail.com>
2023-01-30 00:22:45 +0400

    add antd spinner

--- src/components/Main/TableComponent.jsx
+++ src/components/Main/TableComponent.jsx
@@ -1,28 +1,29 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { UsersSearch } from "../../Redux/Selectors/users";
-import { TableStyle, Tbody } from "../styled-components/table";
+import {useSelector} from "react-redux";
+import {UsersSearch} from "../../Redux/Selectors/users";
+import {TableStyle, Tbody} from "../styled-components/table";
 import ArtistComponent from "./ArtistComponent";
-import Preloader from "./../common/Preloader";
+
+import {Spin} from "antd";
 
 function TableComponent() {
-  const users = useSelector(UsersSearch());
+    const users = useSelector(UsersSearch());
 
-  return (
-    <>
-      {users ? (
-        <TableStyle>
-          <Tbody>
-            {users?.map((item, index) => (
-              <ArtistComponent key={index} item={item} />
-            ))}
-          </Tbody>
-        </TableStyle>
-      ) : (
-        <Preloader />
-      )}
-    </>
-  );
+    return (
+        <>
+            {users ? (
+                <TableStyle>
+                    <Tbody>
+                        {users?.map((item, index) => (
+                            <ArtistComponent key={index} item={item}/>
+                        ))}
+                    </Tbody>
+                </TableStyle>
+            ) : (
+                <Spin/>
+            )}
+        </>
+    );
 }
 
 export default TableComponent;
